7af2fcb187e497fd6e395da17cf4124a136cc746
unknown <saisatwick3000@gmail.com>
2026-01-12 19:30:29 +0530

    Fix login and add role based register

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,32 +2,63 @@ import { useState } from "react"
 import api from "./api"
 
 export default function Login({ setUser }) {
+  const [isRegister, setIsRegister] = useState(false)
+  const [role, setRole] = useState("freelancer")
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
 
   const submit = async (e) => {
-  e.preventDefault()
-  try {
-    const res = await api.post("/api/auth/login", {
-      email,
-      password
-    })
-    setUser(res.data)
-  } catch (error) {
-    console.log(error)
-    setError("Invalid email or password")
+    e.preventDefault()
+    try {
+      if (isRegister) {
+        await api.post("/api/auth/register", {
+          name,
+          email,
+          password,
+          role
+        })
+        setIsRegister(false)
+        setError("Registered successfully. Please login.")
+      } else {
+        const res = await api.post("/api/auth/login", {
+          email,
+          password
+        })
+        setUser(res.data)
+      }
+    // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      setError("Invalid details")
+    }
   }
-}
-
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div style={{ maxWidth: "300px", margin: "auto" }}>
+      <h2>{isRegister ? "Register" : "Login"}</h2>
 
       {error && <p style={{ color: "red" }}>{error}</p>}
 
       <form onSubmit={submit}>
+        {isRegister && (
+          <>
+            <input
+              placeholder="Name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              required
+            />
+            <br />
+
+            <select value={role} onChange={e => setRole(e.target.value)}>
+              <option value="freelancer">Freelancer</option>
+              <option value="client">Client</option>
+            </select>
+            <br />
+          </>
+        )}
+
         <input
           type="email"
           placeholder="Email"
@@ -46,8 +77,14 @@ export default function Login({ setUser }) {
         />
         <br />
 
-        <button type="submit">Login</button>
+        <button type="submit">
+          {isRegister ? "Register" : "Login"}
+        </button>
       </form>
+
+      <button onClick={() => setIsRegister(!isRegister)}>
+        {isRegister ? "Already have an account? Login" : "New user? Register"}
+      </button>
     </div>
   )
 }
