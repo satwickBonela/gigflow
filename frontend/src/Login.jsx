5ea0e257a0135b8bcf822d3763f5d5b6de9a25cd
unknown <saisatwick3000@gmail.com>
2026-01-12 18:05:46 +0530

    Fix catch block in Login

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,18 +4,50 @@ import api from "./api"
 export default function Login({ setUser }) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
 
-  const login = async () => {
-    const res = await api.post("/api/auth/login", { email, password })
+  const submit = async (e) => {
+  e.preventDefault()
+  try {
+    const res = await api.post("/api/auth/login", {
+      email,
+      password
+    })
     setUser(res.data)
+  } catch (error) {
+    console.log(error)
+    setError("Invalid email or password")
   }
+}
+
 
   return (
     <div>
-      <h2>GigFlow Login</h2>
-      <input placeholder="Email" onChange={e=>setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)} />
-      <button onClick={login}>Login</button>
+      <h2>Login</h2>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <form onSubmit={submit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
