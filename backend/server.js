da30b74258a3f42c9b5f9ec64bd0a001c271c2e5
unknown <saisatwick3000@gmail.com>
2026-01-12 16:13:53 +0530

    Fix MongoDb Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,33 @@
+require("dotenv").config()
+
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
-require("dotenv").config()
 
 const app = express()
+
+// Debug: show Mongo URL
+console.log("MONGO_URI =", process.env.MONGO_URI)
+
+// Connect MongoDB (ONLY ONCE)
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Mongo Error:", err))
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }))
+
 app.use(express.json())
 app.use(cookieParser())
 
-mongoose.connect("mongoose.connect(process.env.MONGO_URI")
+app.get("/", (req, res) => res.send("GigFlow API Running"))
 
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err))
-
-app.get("/", (req,res)=>res.send("GigFlow API Running"))
-
-// ROUTES must be before listen
+// Routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/gigs", require("./routes/gigs"))
 app.use("/api/bids", require("./routes/bids"))
-
-
 
 app.listen(5000, () => console.log("Backend running on port 5000"))
