ed725c0a096b1a7af3368987ebfdff3e347efe5a
unknown <saisatwick3000@gmail.com>
2026-01-12 19:36:07 +0530

    Enable role based

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String
+  password: String,
+  role: {
+    type: String,
+    enum: ["client", "freelancer"],
+    default: "freelancer"
+  }
 })
 
 module.exports = mongoose.model("User", userSchema)
