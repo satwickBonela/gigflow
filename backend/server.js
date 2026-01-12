68536c13cb25ec9e65288a0810005a60c7b710dc
unknown <saisatwick3000@gmail.com>
2026-01-12 18:20:33 +0530

    Fix CORS headers

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,7 @@ const cookieParser = require("cookie-parser")
 
 const app = express()
 
-// Debug: show Mongo URL
-console.log("MONGO_URI =", process.env.MONGO_URI)
-
-// Connect MongoDB (ONLY ONCE)
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("Mongo Error:", err))
-
+// 🔥 CORS MUST BE FIRST
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -26,9 +19,12 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Mongo Error:", err))
+
 app.get("/", (req, res) => res.send("GigFlow API Running"))
 
-// Routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/gigs", require("./routes/gigs"))
 app.use("/api/bids", require("./routes/bids"))
